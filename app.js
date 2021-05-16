a690262fa767b017b7ddeaa0e6cc681a6754e4cf
Jacob Turner <jacobturnr@gmail.com>
2021-05-16 20:27:50 +0100

    trying to get sepereate on click to work

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let q = (o)=> document.querySelector(o),
-root = q('.root'),
+rt = q('.root'),
 keys=[1,2,3,4,5,6,7,8,9,'+','-','*','/','='],
 screen=''
-cli= (d)=> console.log(d.target)
-btns=keys.map(e=>root.insertAdjacentHTML("afterbegin", '<button onclick=\'cli()\'>'+e))
+btns=keys.map(e=>rt.insertAdjacentHTML("afterbegin", '<button>'+e))
+rt.onclick(hello)
+function hello(){}
