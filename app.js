02fa4e2a724dab5ad436cb924d496ec6111beb2c
Jacob Turner <jacobturnr@gmail.com>
2021-05-17 02:17:41 +0100

    finished... for now

--- app.js
+++
@@ -1,9 +0,0 @@
-let rt= document.querySelector('div'),
-keys=['=','/','.',0,'C','*',3,2,1,'-',6,5,4,'+',9,8,7],
-screen='',l
-keys.map(e=>rt.insertAdjacentHTML("afterbegin", '<button>'+e))
-rt.addEventListener('click',(e)=>{let i=e.target.innerHTML
-    rt.removeChild(rt.lastChild)
-    i=='C' ? screen='0' :  i=='=' ? l>=0 && !screen.replaceAll('-','').match(/\D{2,}/) ? screen=eval(screen) : console.log('hi') : l=='=' && i>=0 ? screen=i : screen+=i
- l=i
- rt.append(screen)})
--- app2.js
+++
@@ -1,5 +0,0 @@
-let q = o=> document.querySelector(o),
- rt= q('div'), s=q('input'),
- f=  a => a=='C' ? s.value='': a=='=' ? s.value=eval(s.value) : s.value+=a
-k=['=','/','.',0,'C','*',3,2,1,'-',6,5,4,'+',9,8,7]
-k.map(e=>rt.insertAdjacentHTML("afterbegin", "<button onclick=\"(()=>f(&quot;"+e+"&quot;))();\">"+e))
